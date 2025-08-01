864e2741d65a36f5011f39a96c351d8dafe888fa
KISHORE kevin <kishorekevin3289@gmail.com>
2025-08-01 12:16:51 +0530

    Project Update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -23,7 +23,7 @@ export default function TabsLayout() {
 
   return (
       <Tabs screenOptions={{tabBarShowLabel:true,headerShown:false,tabBarStyle:{height:70,backgroundColor:"#B27092",borderTopWidth:0,elevation:0},tabBarActiveTintColor:"#FFE9F3",tabBarInactiveTintColor:"black"}}>
-          <Tabs.Screen name='home'  options={{tabBarLabel:"Search",title:"Search",tabBarIcon:({color})=><Ionicons name='earth-sharp'  color={color} size={24} /> }} />
+          <Tabs.Screen name='index'  options={{tabBarLabel:"Search",title:"Search",tabBarIcon:({color})=><Ionicons name='earth-sharp'  color={color} size={24} /> }} />
           <Tabs.Screen name='map' options={{tabBarLabel:"Map",title:"Map",tabBarIcon:({color})=><Ionicons name='map-sharp'  color={color} size={24} /> }}/>
           <Tabs.Screen name='about' options={{tabBarLabel:"About",title:"About",tabBarIcon:({color})=><Ionicons name='infinite-sharp'  color={color} size={24} /> }} />
       </Tabs>
--- app/(tabs)/map/_layout.tsx
+++ app/(tabs)/map/_layout.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
   export default function MapLayout() {
     return (
       <Stack>
-        <Stack.Screen name='mapview' options={{headerShown:false}} />
+        <Stack.Screen name='index' options={{headerShown:false}} />
         <Stack.Screen name='detail' options={{headerShown:false}} />
       </Stack>
     )
